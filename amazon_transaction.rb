1cabd81e79fce68ce69ba4ef30e63bb469ca1d79
siarasmall <siarasmall19@gmail.com>
2018-07-16 13:35:01 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -17,7 +17,7 @@ class AmazonTransaction
   end 
   
   def apply_discount
-    if @discount
+    if @discount < 0
       @total = @total * ((100.0 - @discount) / 100.0)
       return "After the discount, the total comes to $#{@total.to_i}."
     else return "There is no discount to apply."
